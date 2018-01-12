34d178be78ab57b768d327fcafc22ab0773261b3
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2018-01-12 12:23:11 +0000

    tests finished

--- native/rspec-appium/spec/budget_app_example_spec.rb
+++ native/rspec-appium/spec/budget_app_example_spec.rb
@@ -18,4 +18,31 @@ describe 'Testing the budget app' do
 
   end
 
+  it "should have the budget option on the main landing page" do
+    find_elements(:id, 'protect.budgetwatch:id/menu')[0].displayed?
+
+  end
+
+  it "should access the budget page" do
+    scroll_to('Budgets').click
+    find_elements(:id, 'protect.budgetwatch:id/action_add')[0].displayed?
+
+  end
+
+  it "should be able to add a budget" do
+    find_elements(:id, 'protect.budgetwatch:id/action_add')[0].click
+    find_element(:id, 'protect.budgetwatch:id/budgetNameEdit').send_keys('Test')
+    find_element(:id, 'protect.budgetwatch:id/valueEdit').send_keys('9')
+    find_elements(:id, 'protect.budgetwatch:id/action_save')[0].click
+
+    
+
+
+
+
+
+  end
+
+
+
 end
